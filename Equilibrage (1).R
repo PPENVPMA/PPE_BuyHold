cb9838ff514171ac9041cf84a448f11951c68d8f
PPENVPMA <45937311+PPENVPMA@users.noreply.github.com>
2018-12-17 18:28:26 +0100

    lol

--- Equilibrage (1).R
+++ Equilibrage (1).R
@@ -6,7 +6,7 @@
 # Les tarifs sont notés comme suit: Seuil Minimum,Seuil Maximum,Frais fixe,Frais Variables.
 # les lignes ayant moins d'élément se verront compléter par 0.
 
-d2= read.table("C:/Users/nmace/Documents/ING4/PPE/PPE_BuyHold/Courtiers.txt",header= TRUE, sep=" " )
+d_broker= read.table("Courtiers.txt",header= TRUE, sep=" " )
 ############################### FONCTIONS #################################
 
 # La fonction phy retourne les frais de courtages(frais fixe+ frais variables d'un broker)
@@ -35,21 +35,6 @@ phi = function(m_frais_broker,broker,nb_positions,stock_price_unique)
   return(frais_courtages)
 }
 
-f_nb_actions_seuil = function(objectif,stock_price)
-{
-  nb_action_inf=f_nb_actions(objectif,stock_price)
-  nb_action_sup=f_nb_actions((objectif+stock_price),stock_price)
-  
-  if (objectif-nb_action_inf*stock_price< nb_action_sup*stock_price-objectif)
-  {
-    return(nb_action_inf)
-  }
-  else
-  {
-    return(nb_action_sup)
-  }
-}
-
 f_objectif_nb_actions = function(objectif,stock_price)
 {
   nb_action_inf=objectif%/%stock_price
@@ -66,38 +51,35 @@ f_objectif_nb_actions = function(objectif,stock_price)
 }
 
 f_equilibrage=function(m_frais_broker,broker,objectif,stock_price,nb_action_actuel,cash){
-  
-  nb_action_objectif=f_objectif_nb_actions(objectif,stock_price)
-  
-  variation=nb_action_objectif-nb_action_actuel
-  
-  PnL=0
   nb_stock=nb_action_actuel
-
+  nb_action_objectif=f_objectif_nb_actions(objectif,stock_price)
+  variation=nb_action_objectif-nb_stock
+  PnL=0
+  prix_achat=0
   print(phi(m_frais_broker,broker,variation,stock_price))
-  {
-  
-  if(phi(m_frais_broker,broker,variation,stock_price) <cash){
-    PnL=variation*stock_price-phi(m_frais_broker,broker,variation,stock_price)
+  if(phi(m_frais_broker,broker,variation,stock_price)+abs(variation)*stock_price <cash && variation!=0){
+    prix_achat=-variation*stock_price-phi(m_frais_broker,broker,variation,stock_price)
     nb_stock=nb_stock+variation
-  }}
-  return(c(PnL,nb_stock))
+  }
+  return(c(prix_achat,nb_stock))
 }
 
 
 
 Capital=10000
 Objectif=1000
-stock_price=matrix(runif(n=10,0,100))
-T=10
+stock_price=matrix(c(100,140,100))
+T=3
 nb_stock=0
 
-cash=matrix(Capital,T,ncol=1)
+cash=matrix(0.0,T,ncol=1)
+cash[1]=Capital
 
 for(i in 2:T){
-  val=f_equilibrage(d2,"BoursoramaClassic",Objectif,stock_price[i],nb_stock,cash[i])
+  val=f_equilibrage(d_broker,"BoursoramaClassic",Objectif,stock_price[i],nb_stock,cash[i-1])
   nb_stock=val[2]
-  cash[i]=cash[i]+val[1]
+  cash[i]=cash[i-1]+val[1]
+  print(c(nb_stock,nb_stock*stock_price[i]+cash[i]))
 }
 
 
